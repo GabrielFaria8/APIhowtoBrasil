1c781e12b5ad072e6ba6ecc6d31ad10595784f7e
Kayan33 <kayanpereira33@gmail.com>
2024-04-11 14:04:50 -0300

    ajuste permitir domínio

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://howtobrasil.pvferreira.com.br/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
