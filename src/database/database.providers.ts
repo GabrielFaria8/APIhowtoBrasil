a18a10f879f5818d8747432badf73ac90360a7a2
zardette <150203361+zardette@users.noreply.github.com>
2024-04-10 21:21:29 -0300

    rotas
    .

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,15 +6,17 @@ export const databaseProviders = [
         useFactory: async () => {
             const dataSource = new DataSource({
                 type: 'mysql',
-                host: 'localhost',
+                host: '50.116.112.16',
                 port: 3306,
-                username: 'root',
-                password: '',
-                database: 'apihowtobrasil',
+                username: 'vitali04_howtobrasil',
+                password: 'brasil2306',
+                database: 'vitali04_howtobrasil',
                 entities: [
                 __dirname + '/../**/*.entity{.ts,.js}',
                 ],
                 synchronize: true,
+
+                connectTimeout: 20000, // Tempo limite de 15 segundos (em milissegundos)
             });
             return dataSource.initialize();
         },
